bb8d2eb6634f5051cbff410d66402435db57fa6f
Aidana K <aidana.amnbk@gmail.com>
2025-01-17 15:36:46 +0500

    fix: fix 01 problem, use createReadStream instead of readFile

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,9 +2,17 @@ const path = require('path');
 const fs = require('fs');
 
 const filePath = path.join(__dirname, 'text.txt');
-// console.log(filePath);
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// fs.readFile(filePath, 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+const stream = fs.createReadStream(filePath, 'utf8');
+
+stream.pipe(process.stdout);
+
+stream.on('error', (error) => {
+  console.error('Error reading file:', error.message);
+  process.exit(1);
 });
