99e82daa634a996f712930cc660895798d700763
Aidana K <aidana.amnbk@gmail.com>
2025-01-18 17:06:31 +0500

    feat: iterating through file names in folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,10 @@ const secretForlderPath = path.join(__dirname, 'secret-folder');
 async function getFilesInFolder(fPath) {
   try {
     const files = await fs.readdir(fPath, { withFileTypes: true });
-    console.log(files);
+    // console.log(files);
+    for (let file of files) {
+      console.log(file.name);
+    }
   } catch (error) {
     console.error('Error reading directory:', error.message);
     process.exit(1);
